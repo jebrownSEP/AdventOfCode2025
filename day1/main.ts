8cf5edcc67b5a77ebf401d04466dcb1150e45afe
Joe Brown <jebrown@sep.com>
2025-12-01 14:39:41 -0500

    Day 1 Part 2

--- day1/main.ts
+++ day1/main.ts
@@ -35,48 +35,35 @@ export function part1(rotations: string[]): number {
 
 export function part2(rotations: string[]): number {
   let position = 50;
-  const rotatedPositions = [position];
-  let passOvers = 0;
+
+  let numberOfZeroPositions = 0;
 
   rotations.forEach((rotation) => {
     let movement = parseInt(rotation.substring(1), 10);
 
-    while (movement > 100) {
-      movement -= 100;
-      passOvers += 1;
-    }
+    while (movement > 0) {
+      if (rotation.startsWith('L')) {
+        position = position - 1;
+      } else if (rotation.startsWith('R')) {
+        position = position + 1;
+      } else {
+        throw new Error('Invalid direction ' + rotation);
+      }
+      movement -= 1;
 
-    if (rotation.startsWith('L')) {
-      position = position - movement;
-    } else if (rotation.startsWith('R')) {
-      position = position + movement;
-    } else {
-      throw new Error('Invalid diretion ' + rotation);
-    }
+      if (position > 99) {
+        position = position - 100;
+      } else if (position < 0) {
+        position = position + 100;
+      }
 
-    if (position === 100) {
-      passOvers -= 1;
+      if (position === 0) {
+        numberOfZeroPositions += 1;
+      }
     }
-
-    if (position > 99) {
-      position = position - 100;
-      passOvers += 1;
-    } else if (position < 0) {
-      position = position + 100;
-      passOvers += 1;
-    }
-
-    if (position === 0) {
-      passOvers -= 1;
-    }
-
-    rotatedPositions.push(position);
   });
 
-  console.info(rotatedPositions);
-
-  const zeroPositions = rotatedPositions.filter((pos) => pos === 0);
-  return zeroPositions.length + passOvers;
+  return numberOfZeroPositions;
 }
 
 function main(): void {
